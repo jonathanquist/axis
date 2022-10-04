49d517cb0cd475a94134d262b17cdd214038b5b3
Jonathan Quist <jonathan.quist@outlook.com>
2022-10-04 11:57:18 +0200

    Added global.js and img folder

---
+++ incl/global.js
@@ -0,0 +1,43 @@
+//Global variables
+const cardDeck = document.getElementById('deck');
+const btnAdd = document.getElementById('add');
+const btnRmv = document.getElementById('remove');
+
+//Create initial deck
+const arr = [
+	{"title":"Deck of navigation cards with a large image", "text":"This card has a large image."},
+	{"title":"Network cameras", "text":"This card has a larger image. Nulla erit controversia. Ille enim occurrentia nescio quae."},
+	{"title":"Network cameras", "text":"This card has a larger image. Certe, nisi voluptatem tanti aestimaretis."},
+	{"title":"Network cameras", "text":"This card has a larger image. Er quod est munus, quod opus spientiae?"},
+	{"title":"Network cameras", "text":"This card has a larger image. Sit sane ista volutpas."},
+	{"title":"Network cameras", "text":"This card has a larger image. Omnis enim est natura diligens sui."},
+	{"title":"Network cameras", "text":"This card has a larger image. Ut optime, secundum naturam affectum esse possit. Ut optime, secundum naturam affectum esse possit."},
+];
+	
+for(let i = 0; i<arr.length; i++) {
+	createCard(i);
+}
+
+function createCard(n) {
+	const card = '<a src="#" class="link"><div class="card"><img class="card-top" src="img/card.jpg" alt="Card top"> <div class="card-text"> <h1>'+arr[n].title+'</h1> <p>'+arr[n].text+'</p> </div> </div><div class="card-back-1"> </div><div class="card-back-2"> </div></a>';
+	const tempWrapper = document.createElement('div');
+	tempWrapper.innerHTML = card;
+	cardDeck.appendChild(tempWrapper.firstChild);
+}
+
+//Buttons
+btnAdd.addEventListener("click", add);
+btnRmv.addEventListener("click", remove);
+	
+function add() {
+	if (cardDeck.childElementCount >= 1) {
+		cardDeck.appendChild(cardDeck.firstElementChild.cloneNode(true));
+	} else {
+		createCard(0);
+	}
+}
+	
+function remove() {
+	cardDeck.removeChild(cardDeck.lastChild);
+}
+
